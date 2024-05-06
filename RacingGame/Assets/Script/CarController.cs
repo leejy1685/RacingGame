17550b15249ee285048c6674c1ff9cac39396f2e
leejy1685 <ucc1685@naver.com>
2024-05-06 18:30:59 +0900

    Upload
    초기본

---
+++ RacingGame/Assets/Script/CarController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragOnGround = 3f ;
+
+    private float speedInput, turnInput;
+
+    private bool grounded;
+
+    public LayerMask whatIsGround;
+    public float groundRayLength = .5f;
+    public Transform groundRayPoint;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 25f;
+
+    public ParticleSystem[] dustTrail;
+    public float maxEmission = 25f;
+    private float emissionRate;
+
+
+
+    private void Start()
+    {
+        theRB.transform.parent = null;
+
+    }
+    private void Update()
+    {
+
+        transform.position = theRB.transform.position;
+
+
+        speedInput = 0;
+        if (Input.GetKey("w"))
+        {
+            speedInput = forwardAccel * 1000f;
+        }
+        else if (Input.GetKey("s"))
+        {
+            speedInput =  reverseAccel * 1000f;
+        }
+
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if(grounded)
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime* Input.GetAxis("Vertical"), 0f));
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        emissionRate = 0;
+
+        if (grounded)
+        {
+            theRB.drag = dragOnGround;
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                theRB.AddForce(transform.forward * speedInput);
+                emissionRate = maxEmission;
+            }
+
+        }
+        else
+        {
+            theRB.drag = 0.1f;
+            theRB.AddForce(Vector3.up * -gravityForce * 100f);
+        }
+
+        foreach (ParticleSystem part in dustTrail)
+        {
+            var emissionModule = part.emission;
+            emissionModule.rateOverTime = emissionRate;
+
+        }
+
+
+    }
+
+}
